121beb0677e85bf5a7412599fbe679fdb4ebe7fe
shmuel <shmuel.disraeli@gmail.com>
2022-07-14 15:23:51 +0300

    removing redundent divider

--- src/components/BoxRow.js
+++ src/components/BoxRow.js
@@ -1,16 +1,9 @@
 import React from "react";
 
-const boxRowStyle = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  marginBottom: '10px',
-  paddingBottom: '10px',
-  borderBottom: '1px solid rgba(120, 120, 120, 0.1)'
-}
 
 const BoxRow = props => {
   return (
-    <div style={boxRowStyle}>
+    <div className="box-row">
       {props.children}
     </div>
   )
