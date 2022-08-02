8be3b39418a2ed7f9741b51cef214a8c5ecb59d4
shmuel <shmuel.disraeli@gmail.com>
2022-08-02 11:31:54 +0300

    looping

--- src/pages/Accounts.js
+++ src/pages/Accounts.js
@@ -6,79 +6,106 @@ import mainStore from '../stores/main.store'
 import LiquidationsGraph from '../components/LiquidationsGraph'
 import {whaleFriendlyFormater} from '../components/WhaleFriendly'
 import {removeTokenPrefix} from '../utils'
+import { makeAutoObservable, runInAction } from "mobx"
 
-const columns = [
-  {
-      name: 'Asset',
-      selector: row => row.key,
-      format: row => <b>{removeTokenPrefix(row.key)}</b>,
-      sortable: true,
-      minWidth: '110px'
-  },  
-  {
-      name: 'Total Collateral',
-      selector: row =>  Number(row.total_collateral),
-      format: row => whaleFriendlyFormater(row.total_collateral),
-      sortable: true,
-      minWidth: '140px'
-  },
-  {
-      name: 'Total Debt',
-      selector: row => Number(row.total_debt),
-      format: row => whaleFriendlyFormater(row.total_debt),
-      sortable: true,
-      minWidth: '110px'
-  },
-  {
-      name: 'Median Collateral',
-      selector: row =>  Number(row.median_collateral),
-      format: row => whaleFriendlyFormater(row.median_collateral),
-      sortable: true,
-      minWidth: '140px'
-  },  
-  {
-      name: 'Median Debt',
-      selector: row =>  Number(row.median_debt),
-      format: row => whaleFriendlyFormater(row.median_debt),
-      sortable: true,
-      minWidth: '120px'
-  },  
-  {
-      name: 'Top 10 Accounts Collateral',
-      selector: row =>  Number(row.top_10_collateral),
-      format: row => whaleFriendlyFormater(row.top_10_collateral),
-      sortable: true,
-      minWidth: '140px'
-  },
-  {
-      name: 'Top 10 Accounts Debt',
-      selector: row =>  Number(row.top_10_debt),
-      format: row => whaleFriendlyFormater(row.top_10_debt),
-      sortable: true,
-      minWidth: '140px'
-  },
-  {
-      name: 'Top 1 Account Collateral',
-      selector: row =>  Number(row.top_1_collateral),
-      format: row => whaleFriendlyFormater(row.top_1_collateral),
-      sortable: true,
-      minWidth: '140px'
-  },
-  {
-      name: 'Top 1 Account Debt',
-      selector: row =>  Number(row.top_1_debt),
-      format: row => whaleFriendlyFormater(row.top_1_debt),
-      sortable: true,
-      minWidth: '140px'
-  },
-]
+
+class LocalStore {
+  looping = false
+
+  constructor (){
+    makeAutoObservable(this)
+  }
+
+  toggleLooping = () => {
+    this.looping = !this.looping
+    console.log("toggleLooping", this.looping)
+  }
+
+  get loopingPrefix(){
+    return !this.looping ? 'nl_' : ''
+  }
+
+  prefixLooping = str => {
+     return this.loopingPrefix + str
+  }
+}
+
+const localStore = new LocalStore()
 
 const rowPreExpanded = row => row.defaultExpanded
   
 class Accounts extends Component {
   render (){
+    const {prefixLooping} = localStore
+    const columns = [
+      {
+          name: 'Asset',
+          selector: row => row.key,
+          format: row => <b>{removeTokenPrefix(row.key)}</b>,
+          sortable: true,
+          minWidth: '110px'
+      },  
+      {
+          name: 'Total Collateral',
+          selector: row =>  Number(row[prefixLooping('total_collateral')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('total_collateral')]),
+          sortable: true,
+          minWidth: '140px'
+      },
+      {
+          name: 'Total Debt',
+          selector: row => Number(row[prefixLooping('total_debt')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('total_debt')]),
+          sortable: true,
+          minWidth: '110px'
+      },
+      {
+          name: 'Median Collateral',
+          selector: row =>  Number(row[prefixLooping('median_collateral')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('median_collateral')]),
+          sortable: true,
+          minWidth: '140px'
+      },  
+      {
+          name: 'Median Debt',
+          selector: row =>  Number(row[prefixLooping('median_debt')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('median_debt')]),
+          sortable: true,
+          minWidth: '120px'
+      },  
+      {
+          name: 'Top 10 Accounts Collateral',
+          selector: row =>  Number(row[prefixLooping('top_10_collateral')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('top_10_collateral')]),
+          sortable: true,
+          minWidth: '140px'
+      },
+      {
+          name: 'Top 10 Accounts Debt',
+          selector: row =>  Number(row[prefixLooping('top_10_debt')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('top_10_debt')]),
+          sortable: true,
+          minWidth: '140px'
+      },
+      {
+          name: 'Top 1 Account Collateral',
+          selector: row =>  Number(row[prefixLooping('top_1_collateral')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('top_1_collateral')]),
+          sortable: true,
+          minWidth: '140px'
+      },
+      {
+          name: 'Top 1 Account Debt',
+          selector: row =>  Number(row[prefixLooping('top_1_debt')]),
+          format: row => whaleFriendlyFormater(row[prefixLooping('top_1_debt')]),
+          sortable: true,
+          minWidth: '140px'
+      },
+    ]
+
     const loading = mainStore['accounts_loading']
     const rawData = mainStore['accounts_data'] || {}
+    
     const {json_time} = rawData
     if(json_time){
       delete rawData.json_time
@@ -98,6 +125,12 @@ class Accounts extends Component {
     
     return (
       <div>
+        <fieldset>
+          <label for="switch">
+            <input onChange={localStore.toggleLooping} value={localStore.looping} type="checkbox" id="switch" name="switch" role="switch"/>
+            <span className="nudge" data-tooltip="same asset used as debt and collateral">looping</span>
+          </label>
+        </fieldset>
         <Box loading={loading} time={json_time}>
           {!loading && <DataTable
               expandableRows
