bee9fcff9f4e0af4584065ce91524ed4d731e8b9
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-09-26 10:48:50 +0200

    setup for gearbox

---
+++ public/configs/gearbox.js
@@ -0,0 +1,10 @@
+window.APP_CONFIG = {
+    PLATFORM_ID: '3',
+    TOKEN_PREFIX: '',
+    BLOCK_EXPLORER: 'https://etherscan.com',
+    WHITE_LOGO: 'aurigami.svg',
+    BLACK_LOGO: 'aurigami.svg',
+    feature_flags: {
+      alerts: false
+    },
+  }
