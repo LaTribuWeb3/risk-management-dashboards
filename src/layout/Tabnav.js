b075ed1fe63fca53b6e088a956b88a0a780997ae
Alfulinku <9356022+alfulinku@users.noreply.github.com>
2022-09-27 10:16:15 +0200

    Update Tabnav.js

--- src/layout/Tabnav.js
+++ src/layout/Tabnav.js
@@ -1,27 +1,44 @@
-import React, { useEffect, Component } from "react"
+import React, {Component} from "react"
 import {observer} from "mobx-react"
 import poolsStore from "../stores/pools.store"
-import { tokenName } from "../utils"
+import alertStore from '../stores/alert.store'
+import mainStore from "../stores/main.store"
 
-const Tabnav = (props) => {
-// class Tabnav extends Component {
-  // render (){
-  const loading = poolsStore['pools_loading'] || poolsStore['data/tokens?fakeMainnet=0_loading'];
-  const poolsData = Object.assign([], poolsStore['pools_data'] || []);
-  const tokenData = Object.assign([], poolsStore['data/tokens?fakeMainnet=0_data'] || []);
-  
-  return (
-    <div className="tabnav">
-    <select>
-      {loading ? <span>loading pools...</span> : poolsData.map((pool, i) => {
-          const symbol = tokenData.find(t => t.address === pool.underlying)?.symbol;
-          return <option key={i} value={symbol} selected= {i === 0}>{symbol + ' pool'}</option>
-      })
+// const Tabnav = (props) => {
+class Tabnav extends Component {
+  render (){
+    const loading = poolsStore['data/tokens?fakeMainnet=0_loading'] || poolsStore['pools_loading'];
+    const poolsData = Object.assign([], poolsStore['pools_data'] || []);
+    const tokenData = Object.assign([], poolsStore['data/tokens?fakeMainnet=0_data'] || []);
+    
+    function onPoolSelect(event) {
+      const selectedLabel = event.target.selectedOptions[0].label;
+      const poolAddress = event.target.selectedOptions[0].value;
+      alertStore.valueAtRisk = poolAddress;
+
+      // mainStore['account_data'] = poolsStore['pool_data']['0x7407180AE470113761d3EC99Aa973cf8Bd127c68'];
+
+      if(selectedLabel.includes('DAI')) {
+        alertStore.liquidationsAtRisk = 100;
+      } else if(selectedLabel.includes('USDC')) {
+        alertStore.liquidationsAtRisk = 89999;
+      } else {
+        alertStore.liquidationsAtRisk = 20050;
+      }
     }
-    </select>
-    </div>
-  )
+
+    return (
+      <div className="tabnav">
+      <select onChange={onPoolSelect}>
+        {loading ? <span>loading...</span> : poolsData.map((pool, i) => {
+            const symbol = tokenData.find(t => t.address === pool.underlying)?.symbol;
+            return <option key={i} value={pool.address} /*select={i === 0}*/>{symbol + ' pool'}</option>
+        })
+      }
+      </select>
+      </div>
+    )
+  }
 }
-// }
 
 export default observer(Tabnav)
