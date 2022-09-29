f720c0c84c95703e6b485405e53a37594b68299b
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-09-29 11:04:38 +0200

    added gearbox logo

--- public/configs/gearbox.js
+++ public/configs/gearbox.js
@@ -2,8 +2,8 @@ window.APP_CONFIG = {
   PLATFORM_ID: "3",
   TOKEN_PREFIX: "",
   BLOCK_EXPLORER: "https://etherscan.com",
-  WHITE_LOGO: "aurigami.svg",
-  BLACK_LOGO: "aurigami.svg",
+  WHITE_LOGO: "gearbox.png",
+  BLACK_LOGO: "gearbox.png",
   feature_flags: {
     alerts: false,
   },
