f44e323e5026986da8ee62c0299b28d80b784fb6
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-10-05 14:25:35 +0200

    implemented two utility functions (tokenName, tokenPrice)

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@ import { TOKEN_PREFIX } from "./constants";
 import React from "react";
 import poolsStore from "./stores/pools.store";
 import { relativeTimeRounding } from "moment";
+import BigNumber from "bignumber.js";
+
 
 export const removeTokenPrefix = (token) => token.replace(TOKEN_PREFIX, "");
 
@@ -15,14 +17,37 @@ export const precentFormatter = (num) => {
   return (num * 100).toFixed(2) + "%";
 };
 
-const tokenData = Object.assign(
-  [],
-  poolsStore["data/tokens?fakeMainnet=0_data"] || []
-);
+
 export const tokenName = (address) => {
+  const tokenData = Object.assign(
+    [],
+    poolsStore["data/tokens?fakeMainnet=0_data"] || []
+  );
   for (const token in tokenData) {
     if (tokenData[token].address.toLowerCase() === address.toLowerCase()) {
       return tokenData[token].symbol;
+    }
+  }
+};
+
+
+export const tokenPrice = (symbol, amount) => {
+  if(amount == 0){
+    return 0
+  }
+  const tokenData = Object.assign(
+    [],
+    poolsStore["data/tokens?fakeMainnet=0_data"] || []
+  );
+  for (const token in tokenData) {
+    if (tokenData[token].symbol.toLowerCase() === symbol.toLowerCase()) {
+      const tokenPrice = BigNumber(tokenData[token].priceUSD18Decimals).div(BigNumber(10).pow(18));
+      const tokenDecimals = tokenData[token].decimals;
+      const tokenAmount = BigNumber(amount).div(
+        BigNumber(10).pow(tokenDecimals)
+      );
+      const result = Math.floor(BigNumber(tokenPrice).multipliedBy(BigNumber(tokenAmount)));
+      return result.toString();
     }
   }
 };
