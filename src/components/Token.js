c674fbf32f96c6d782a8c2450bb2d8c826ae8c8e
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-10-10 13:37:00 +0200

    deleted conversion

--- src/components/Token.js
+++ src/components/Token.js
@@ -1,12 +1,8 @@
-import React from "react";
 import { observer } from "mobx-react";
 import { removeTokenPrefix } from "../utils";
 
 const Token = (props) => {
   let name = removeTokenPrefix(props.value);
-  if (name === "WETH") {
-    name = "ETH";
-  }
   return <b>{name}</b>;
 };
 
