41770acd58286bdd2e2b020d0dcbe1dfaed16591
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-10-13 10:11:18 +0200

    sandbox according to wireframe

--- src/pages/Simulation.js
+++ src/pages/Simulation.js
@@ -1,13 +1,12 @@
-import React, { Component } from "react";
-import { observer } from "mobx-react";
 import Box from "../components/Box";
-import DataTable from "react-data-table-component";
-import mainStore from "../stores/main.store";
-import { removeTokenPrefix } from "../utils";
 import CapInput from "../components/CapInput";
 import CfDiff from "../components/CfDiff";
-import riskStore from "../stores/risk.store";
+import { Component } from "react";
+import DataTable from "react-data-table-component";
 import Token from "../components/Token";
+import mainStore from "../stores/main.store";
+import { observer } from "mobx-react";
+import riskStore from "../stores/risk.store";
 
 const columns = [
   {
@@ -16,20 +15,14 @@ const columns = [
     format: (row) => <Token value={row.asset} />,
   },
   {
-    name: "Supply Cap",
+    name: "Supply",
     selector: (row) => row.mint_cap,
     format: (row) => <CapInput row={row} field={"mint_cap"} />,
     grow: 2,
   },
   {
-    name: "Borrow Cap",
-    selector: (row) => row.borrow_cap,
-    format: (row) => <CapInput row={row} field={"borrow_cap"} />,
-    grow: 2,
-  },
-  {
     name: "Current Collateral Factor",
-    selector: (row) => riskStore.getCurrentCollateralFactor(row.asset),
+    selector: (row) => row.currentLT,
     width: "260px",
   },
   {
@@ -40,54 +33,10 @@ const columns = [
   },
 ];
 
-const expendedBoxStyle = {
-  margin: "30px",
-  width: "100%",
-  minHeight: "100px",
-  padding: "30px",
-};
-const humanizeRecommendation = (r) => {
-  const rItems = r.split(" ");
-  rItems[1] = removeTokenPrefix(rItems[1]);
-  if (rItems[2] === "mint") {
-    rItems[2] = "supply";
-  }
-  return rItems.join(" ");
-};
-
-const Recommendation = (props) => {
-  let recommendations = [];
-  riskStore.recommendations.forEach((r) => {
-    if (r.asset === props.data.asset) {
-      recommendations.push(r.recommendation);
-    }
-  });
-  recommendations = [...new Set(recommendations)];
-  return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "space-between",
-        width: "100%",
-      }}
-    >
-      <article style={expendedBoxStyle}>
-        <h6>to improve collateral factor</h6>
-        {recommendations.map((r) => (
-          <div key={r}>
-            <a onClick={() => riskStore.preformRecommendation(r)}>
-              {humanizeRecommendation(r)}
-            </a>
-          </div>
-        ))}
-      </article>
-    </div>
-  );
-};
 
 class Simulation extends Component {
   render() {
-    const { loading } = riskStore;
+    const { loading } = riskStore["currentRiskLoading"];
     const { json_time } = mainStore["risk_params_data"] || {};
     return (
       <div>
@@ -95,10 +44,8 @@ class Simulation extends Component {
           {/* <h6>Risk Parameters Recommendations</h6> */}
           {!loading && (
             <DataTable
-              expandableRows
               columns={columns}
-              data={riskStore.data}
-              expandableRowsComponent={Recommendation}
+              data={riskStore.currentRiskData}
             />
           )}
         </Box>
