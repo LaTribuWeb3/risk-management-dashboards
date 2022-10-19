45cd18259f446607b82e864cdb40b96c16caf56c
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-10-19 09:56:08 +0200

    Fixed sandbox text

--- src/pages/SinglePage.js
+++ src/pages/SinglePage.js
@@ -84,10 +84,7 @@ class SinglePage extends Component {
                   <hgroup>
                     <h2> Risk Parameters Sandbox</h2>
                     <p className="description">
-                      The sandbox lets you set different Supply and Borrow caps
-                      to get Collateral Factor recommendations according to
-                      different caps. The tool also provides optimization
-                      setting recommendations.{" "}
+                    The sandbox lets you set different Supply caps, to get LTs recommendations according to different caps.{" "}
                     </p>
                   </hgroup>
                   <Simulation />
