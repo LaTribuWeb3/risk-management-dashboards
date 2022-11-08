7a3718f79aa44108c19fb9ade2411db9725c63f5
0xVorian <33686588+0xVorian@users.noreply.github.com>
2022-11-08 10:13:11 +0100

    better lastUpdated

--- src/components/BoxFooter.js
+++ src/components/BoxFooter.js
@@ -10,17 +10,21 @@ const BoxFooter = (props) => {
     //Get Days and subtract from duration
     const days = duration.days();
     duration.subtract(days, "days");
+    const dayStr = days ? (days + " day" + (days > 1 ? "s," : ",") ) : ""
 
     //Get hours and subtract from duration
     const hours = duration.hours();
     duration.subtract(hours, "hours");
+    const hourStr = days ? (hours + " hour" + (hours > 1 ? "s," : ",") ) : ""
 
     //Get Minutes and subtract from duration
     const minutes = duration.minutes();
     duration.subtract(minutes, "minutes");
+    const minuteStr = minutes ? (minutes + " minute" + (minutes > 1 ? "s," : ",") ) : ""
 
     //Get seconds
     const seconds = duration.seconds();
+    const secondStr = seconds ? (seconds + " second" + (seconds > 1 ? "s" : "")) : ""
 
     return (
       <div
@@ -34,10 +38,7 @@ const BoxFooter = (props) => {
         }}
       >
         <small>
-          last updated {days ? days + " days, " : ""}{" "}
-          {hours ? hours + " hours, " : ""}
-          {minutes ? minutes + " minutes, " : ""}{" "}
-          {seconds ? seconds + " seconds ago" : ""}
+          last updated {dayStr} {hourStr} {minuteStr} {secondStr} ago.
         </small>
       </div>
     );
